6d2c912eef2029a8da3e1449b27e03fbf90f6822
minbo2002 <minbo2002@naver.com>
2023-04-27 18:44:23 +0900

    feat: 첫프로젝트 환경셋팅 및 런타임테스트

---
+++ src/main/java/com/tennis/matching/TennisProjectApplication.java
@@ -0,0 +1,13 @@
+package com.tennis.matching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TennisProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tennis/matching/TennisProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tennis.matching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
